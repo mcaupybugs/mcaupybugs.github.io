58d3099d96936fc485a206ffc2fc5e11b2c294f8
mcaupybugs <vishalydv.me@gmail.com>
2020-08-23 03:11:55 +0530

    Added web dev

--- src/components/WebDev.jsx
+++ src/components/WebDev.jsx
@@ -1,49 +1,49 @@
 import React from "react";
-var bunkit = require("../assets/bunkit.webp");
-var CodingHub = require("../assets/codinghub.webp");
+var chicken = require("../assets/chicken.png");
+var marsRover = require("../assets/marsRover.png");
 
-export default class Android extends React.Component {
+export default class WebDev extends React.Component {
   render() {
     return (
       <div>
         <div
-          id="android"
+          id="web"
           class="position-relative p-3 p-md-5 m-md-3 text-center blackColor text-light"
         >
           <div className="position-absolute">
-            <img height="600" src={CodingHub}></img>
+            <img src={chicken}></img>
           </div>
           <div
             style={{ right: "5%", top: "9%" }}
             className=" position-absolute"
           >
-            <img height="600" src={bunkit}></img>
+            <img src={marsRover}></img>
           </div>
           <div class="col-md-5 p-lg-5 mx-auto my-5">
             <h1 class="display-5 text-light font-weight-normal">
-              Android Apps{" "}
+              Web Development
             </h1>
             <p class="lead text-light font-weight-normal">
               <ul class="text-light justify-content-center list-group list-group-horizontal-xl">
                 <li class="blackColor text-light list-group-item">
-                  CodingHub - Contest Notifier
-                  <div>
+                  Restaurant App
+                  <div class="text-light">
                     <a
                       className="btn txt-light"
-                      href="https://play.google.com/store/apps/details?id=com.mcaupybugs.codinghubb"
+                      href="https://taimur-ka-dhaba.herokuapp.com/  "
                     >
-                      <span class="text-light">Get it here!</span>
+                      <span class="text-light">Visit it here!</span>
                     </a>
                   </div>
                 </li>
                 <li class="blackColor text-light list-group-item">
-                  BunkIT - Attendance Manager
+                  Mars Rover
                   <div>
                     <a
                       className="btn txt-light"
-                      href="https://play.google.com/store/apps/details?id=com.mcaupybugs.bunkit"
+                      href="https://mbugs.herokuapp.com/"
                     >
-                      <span class="text-light">Get it here!</span>
+                      <span class="text-light">Visit it here!</span>
                     </a>
                   </div>
                 </li>
@@ -52,20 +52,28 @@ export default class Android extends React.Component {
             <p class="lead text-light font-weight-normal">
               <ul class="text-light justify-content-center list-group list-group-horizontal-xl">
                 <li class="blackColor text-light list-group-item">
-                  CodingHub is a contest notifier app that notifies you about
-                  upcoming contests and you can also set a reminder a contest
-                  before it begins. The app supports multiple websites including
-                  codechef,codeforces and even google.
+                  Restaurant App made with Node.JS along with MongoDB database.
+                  Notifies on correct order using SendGrid API and has password
+                  authentication using passport!!
                 </li>
                 <li class="blackColor text-light list-group-item">
-                  BunkIT - Attendance Manager is an app that will record you
-                  attendance, notify you when your attendance is low and even
-                  record your assignments for you so that you never miss a
-                  deadline. With calendar mode, history mode and online sync
-                  available this app is one place for all academic help!!
+                  Mars Rover was a microsoft project made in a team of two along
+                  with Mavneet Kaur. We were able to visualise shortest path
+                  algorithm that a rover will have to apply to find shortest
+                  path. We took care of many obstacles on the way. Have a look
+                  and try yourself!
                 </li>
               </ul>
             </p>
+            <p class="lead text-light font-weight-normal">
+              More projects can be found on my github page!!
+            </p>
+            <a
+              class="btn btn-light btn-outline-secondary"
+              href="https://github.com/mcaupybugs"
+            >
+              Github
+            </a>
           </div>
         </div>
         <div></div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import PageBody from "../components/PageBody";
 import Android from "../components/Android";
+import WebDev from "../components/WebDev";
 
 export default class Homepage extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export default class Homepage extends React.Component {
         <Navbar></Navbar>
         <PageBody></PageBody>
         <Android></Android>
+        <WebDev></WebDev>
       </div>
     );
   }
